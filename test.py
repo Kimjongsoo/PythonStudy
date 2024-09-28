83b93560c2cc43ebea422c6486eac67ed1b35dc5
Kimjongsoo <31787166+Kimjongsoo@users.noreply.github.com>
2024-09-28 22:33:10 +0900

    eidt

--- test.py
+++ test.py
@@ -10,9 +10,6 @@ class MyWindow(QMainWindow, form_class):
         super().__init__()
         self.setupUi(self)
 
-        self.pushbutton.clicked.connect(self.show_message)
-
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     myWindow = MyWindow()
